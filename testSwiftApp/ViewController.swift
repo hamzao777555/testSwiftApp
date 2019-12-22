3a0e4d8faa2f3a5960ac8714b774dd340422cde5
abokhalel2 <abokhalel2@gmail.com>
2019-12-22 13:57:27 -0600

    إضافة جديدة

--- testSwiftApp/ViewController.swift
+++ testSwiftApp/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hello on Github!")
+        print("hello on Github!")
         // Do any additional setup after loading the view.
     }
 
